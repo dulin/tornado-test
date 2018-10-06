8ceeb9f2b50b4cd0f18baa9149140721feec1925
Martin Dulin <martin@dulin.me.uk>
2018-10-06 23:27:49 +0100

    Update .dockerignore, update dockerfiles

--- docker/app/devel-entrypoint.sh
+++ docker/app/devel-entrypoint.sh
@@ -3,7 +3,8 @@
 echo "Running ${0}"
 pip install -e .
 
-find ./app/ -name '*.pyc' -exec rm -f {} \;
+#find ./app/ -name '*.pyc' -exec rm -f {} \;
+#find . -name __pycache__ -type d -exec rm -fr {} \;
 
 echo "Calling exec $*"
 exec "$@"
